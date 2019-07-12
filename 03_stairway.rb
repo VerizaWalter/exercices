4e1559eb3557b89f450ba5facdbc7a822607fe5c
VerizaWalter <verizawalter@gmail.com>
2019-07-12 08:53:53 +0300

    mise à jour du code stairway

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,37 +1,41 @@
-def play
- puts "Bienvenue dans le jeu!!"  step = 0
- show_state(step)  while(!is_over?(step)) do
-   puts "tapez un chiffre entre 1 et 6 pour jouer"
-   gets.chomp.to_i
-    step += analyze_dice(roll_dice)
-   show_state(step)
- end  puts "===Vous avez gagné!==="
-end
-
 def roll_dice
- return rand(6)
+  return rand(6)+1
 end
-
 def analyze_dice(dice)
- if dice == 5 || dice ==6
-   puts "Vous avancez!"
-   return 1
- elsif dice == 1
-   puts "Vous reculez!"
-   return -1
- else dice == 2 || dice == 3 || dice ==4
-   puts "rien ne se passe"
-   return 0
- end
+  if dice >=5
+    puts "Vous avancez!"
+    return 1
+  elsif dice == 1
+    puts "Vous reculez!"
+    return -1
+  else
+    puts "rien ne se passe"
+    return 0
+  end
 end
-
 def show_state(num)
- puts "Vous êtes sur la marche n° #{num}"
-enddef is_over?(num)
- if num == 10
-   return true
- else
-   return false
- end
+  puts "Vous êtes sur la marche n° #{num}"
+end
+def is_over?(num)
+  if num == 10
+    return true
+  else
+    return false
+  end
+end
+def play
+  puts "Bienvenue dans le dé magic!!"
+
+  step = 1
+  show_state(step)
+
+  while(!is_over?(step)) do
+    puts "tapez 'entrée' pour jouer"
+    gets.chomp
+    step += analyze_dice(roll_dice)
+    show_state(step)
+  end
+
+  puts "Bravo, Vous avez gagné! :-)"
 end
 play
